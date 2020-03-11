21a04a9af88a3a7c4a8da3dba46807a3dfb026ea
János Szomor <jszomor@organicawater.com>
2020-03-11 17:04:38 +0100

    Add project files.

---
+++ CsereTorvenyFrameWork/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsereTorvenyFrameWork
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = "itv.txt";
+			string inputCopy = "itvCopy.txt";
+			//FileStream openFile = new FileStream(input, FileMode.Open, FileAccess.ReadWrite);
+			Encoding e = Encoding.GetEncoding(1250);
+			string beolvasottSor;
+			string ujsor;
+			string rendelet;
+			using (StreamReader fileToRead = new StreamReader(input, e))
+			{
+				StreamWriter fileToWrite = new StreamWriter(inputCopy, true, e);
+
+				while ((beolvasottSor = fileToRead.ReadLine()) != null)
+				{
+					ujsor = "";
+					rendelet = beolvasottSor.Replace("törvény", "rendelet");
+					ujsor += rendelet;					
+					fileToWrite.WriteLine(ujsor);
+				}
+				fileToWrite.Close();
+			}
+		}
+	}
+}
